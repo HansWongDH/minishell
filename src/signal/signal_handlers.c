2c3b533c305768d94aeb2d2cc505374cd0654686
Eason Chai <echai@student.42kl.edu.my>
2022-06-22 14:32:05 +0800

    Fix signal

--- src/signal/signal_handlers.c
+++ src/signal/signal_handlers.c
@@ -6,7 +6,7 @@
 /*   By: echai <echai@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 10:16:04 by echai             #+#    #+#             */
-/*   Updated: 2022/06/22 14:02:30 by echai            ###   ########.fr       */
+/*   Updated: 2022/06/22 14:31:40 by echai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ctrl_c(int signo)
 {
 	(void)signo;
-	write(1, "\n", 3);
+	write(1, "\n", 1);
 	rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
